0129eaa9fe4fc88994c9b4366351b4388146ace7
ddnnss <ddnnss.i1@gmail.com>
2019-05-31 00:13:59 +0300

    new pages

--- content/urls.py
+++ content/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('news/<slug>', views.news, name='news'),
+    path('flour/<level>', views.flour, name='flour'),
 
 
 
--- content/views.py
+++ content/views.py
@@ -5,4 +5,26 @@ from .models import *
 
 def index(request):
     last_news = News.objects.all()[:3]
+    albums = PhotoAlbum.objects.filter(show_at_homepage=True)[:3]
     return render(request, 'content/index.html', locals())
+
+def about(request):
+    return render(request, 'content/about.html', locals())
+
+def flour(request,level):
+    flour_level = level
+    prev_level = int(level) - 1
+    next_level = int(level) + 1
+    if level == '2':
+        prev_level = 0
+    elif level == '13':
+        next_level = 0
+    return render(request, 'content/flour.html', locals())
+
+def news(request,slug):
+    if slug == 'all':
+        news = News.objects.all()
+        return render(request, 'content/news.html', locals())
+    else:
+        news_item = News.objects.get(slug=slug)
+        return render(request, 'content/news_single.html', locals())
